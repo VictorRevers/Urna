e09786dd9719b951cff9c79f6d01a8b162113fa6
VictorRevers <victorrkasnowski@gmail.com>
2020-09-20 00:30:27 -0300

    Ucommit

---
+++ js/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '../images/38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEF',
+                fotos:[
+                    {url: '../images/77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'ABC',
+                vice: 'Zul',
+                fotos: [
+                    {url:'../images/84.jpg', legenda:'Prefeito'},
+                    {url:'../images/84-vice.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '94',
+                nome: 'Ciclano',
+                partido: 'DEF',
+                vice: 'Cic',
+                fotos: [
+                    {url:'../images/94.jpg', legenda:'Prefeito'},
+                    {url:'../images/94-vice.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+
+        ]
+    }
+];
